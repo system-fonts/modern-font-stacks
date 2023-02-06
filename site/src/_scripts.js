4b4260d55257fbc2d70bf4b1cb35e663a5d242da
Dan Klammer <danklammer@gmail.com>
2023-02-05 20:13:23 -0500

    Smooth Scroll only on #anchor links

--- site/src/_scripts.js
+++ site/src/_scripts.js
@@ -155,3 +155,12 @@ document.addEventListener('click', event => {
     menu.removeAttribute("open");
   }
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
